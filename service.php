6a46f92633c6649245a5ac4d874809004dc1e714
jose grados <gym2397@gmail.com>
2022-02-18 21:51:03 -0500

    servicio actualizado

--- service.php
+++ service.php
@@ -9,6 +9,10 @@
 
   <!-- Bootstrap CSS v5.0.2 -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  
+  <!-- locomotive -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/locomotive-scroll@4.1.1/dist/locomotive-scroll.min.css">
+  
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/service.css">
 
@@ -16,81 +20,90 @@
 
 <body>
   <?php include('header-temp.html'); ?>
-
-  <div class="container-fluid dp-cont-main">
-    <div class="row dp-bg">
-      <div class="cont-text">
-        <div class="p">Lorem ipsum, dolor sit amet consectetur adipisicing
-          elit.alias amet possimus tenetur iste illo sit aut.
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="container dp-cont-pt1">
+  <div class="container-fluid p-0" data-scroll-container>
     <!-- <div class="row"> -->
-      <div class="col-md align-self-center">
-        <div class="h6 dp-sb-title">BRINDAMOS LOS MEJORES SERVICIOS</div>
-        <div class="h5 dp-sb-title">CONOCE LO QUE TE OFRECEMOS</div>
-        <div class="p">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, laboriosam ratione accusamus libero quasi perferendis debitis earum dolorem officiis a accusantium provident aliquam voluptas ad fuga deleniti? Debitis, possimus tempore?
+      <div class="row dp-cont-main" data-scroll-section>
+        <div class="row dp-bg">
+          <div class="cont-text">
+            <div class="p">Lorem ipsum, dolor sit amet consectetur adipisicing
+              elit.alias amet possimus tenetur iste illo sit aut.
+            </div>
+          </div>
         </div>
       </div>
-      <div class="col-md dp-cont-img">
-        <img src="https://images.pexels.com/photos/3184436/pexels-photo-3184436.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" srcset="">
+  
+      <div class="container dp-cont-pt1" data-scroll-section>
+        <!-- <div class="row"> -->
+          <div class="col-md align-self-center">
+            <div class="h6 dp-sb-title">BRINDAMOS LOS MEJORES SERVICIOS</div>
+            <div class="h5 dp-sb-title">CONOCE LO QUE TE OFRECEMOS</div>
+            <div class="p">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, laboriosam ratione accusamus libero quasi perferendis debitis earum dolorem officiis a accusantium provident aliquam voluptas ad fuga deleniti? Debitis, possimus tempore?
+            </div>
+          </div>
+          <div class="col-md dp-cont-img">
+            <img src="https://images.pexels.com/photos/3184436/pexels-photo-3184436.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" srcset="">
+          </div>
+        <!-- </div> -->
       </div>
+  
+      <div class="container dp-cont-pt2" data-scroll-section>
+        <div class="col-md-12 row">
+          <div class="col-md-6 dp-block" id="cont-serv">
+            <div class="col-md">
+              <div class="h5 dp-sb-title">Gestión de importaciones y aduanas</div>
+              <div class="cont-text">
+                <div class="p">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat quod facere laboriosam ducimus iste quisquam dolores molestiae harum, sit minus sint architecto fuga, eveniet explicabo maxime quaerat aliquam rem consequatur.</div>
+              </div>
+            </div>
+            <div class="col-md">
+              <div class="h5 dp-sb-title">Servicio 2 </div>
+              <div class="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. In ex deserunt sunt cupiditate expedita non unde, molestias mollitia porro explicabo aut, necessitatibus beatae quo minus ipsa! Non, expedita. Blanditiis, libero.</div>
+            </div>
+            <div class="col-md">
+              <div class="h5 dp-sb-title">Servicio 3</div>
+              <div class="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. In ex deserunt sunt cupiditate expedita non unde, molestias mollitia porro explicabo aut, necessitatibus beatae quo minus ipsa! Non, expedita. Blanditiis, libero.</div>
+            </div>
+            <div class="col-md">
+              <div class="h5 dp-sb-title">Servicio 4 </div>
+              <div class="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. In ex deserunt sunt cupiditate expedita non unde, molestias mollitia porro explicabo aut, necessitatibus beatae quo minus ipsa! Non, expedita. Blanditiis, libero.</div>
+            </div>
+          </div>
+          <div class="col-md-6 dp-block ">
+            <div class="col-md block-img"
+                data-scroll data-scroll-sticky data-scroll-target="#cont-serv" data-scroll-speed="1">
+              <img src="https://images.unsplash.com/photo-1524178232363-1fb2b075b655?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fGJ1c2luZXNzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" srcset="">
+            </div>
+          <!--  <div class="col-md">
+              <img src="https://media.istockphoto.com/photos/fountain-pen-signature-picture-id485827074?k=20&m=485827074&s=612x612&w=0&h=3E1wvSupDtQMuwHn2beoOvYhUhm2z9KjGKgkeaMoG14=" alt="" srcset="">
+            </div>
+            <div class="col-md">
+              <img src="https://us.123rf.com/450wm/rastudio/rastudio1604/rastudio160406624/55076297-cohete-mano-dibujada-en-tiza-vector-icono-aislado-para-web-m%C3%B3vil-e-infograf%C3%ADa-.jpg?ver=6" alt="" srcset="">
+            </div> -->
+          </div>
+        </div>
+      </div>
+  
+      <?php include('footer-temp.html'); ?>
     <!-- </div> -->
   </div>
 
-  <div class="container dp-cont-pt2">
-    <div class="col-md-12 row">
-      <div class="col-md-6 dp-block">
-        <div class="col-md">
-          <div class="h6 dp-sb-title">Gestión de importaciones y aduanas</div>
-          <div class="cont-text">
-            <div class="p">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat quod facere laboriosam ducimus iste quisquam dolores molestiae harum, sit minus sint architecto fuga, eveniet explicabo maxime quaerat aliquam rem consequatur.</div>
-          </div>
-        </div>
-        <div class="col-md">
-          <div class="h6 dp-sb-title">Gestión de </div>
-          <div class="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. In ex deserunt sunt cupiditate expedita non unde, molestias mollitia porro explicabo aut, necessitatibus beatae quo minus ipsa! Non, expedita. Blanditiis, libero.</div>
-        </div>
-        <div class="col-md">
-          <div class="h6 dp-sb-title">Gestión de </div>
-          <div class="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. In ex deserunt sunt cupiditate expedita non unde, molestias mollitia porro explicabo aut, necessitatibus beatae quo minus ipsa! Non, expedita. Blanditiis, libero.</div>
-        </div>
-      </div>
-      <div class="col-md-6 dp-block">
-        <div class="col-md">
-          <img src="https://us.123rf.com/450wm/rastudio/rastudio1604/rastudio160406624/55076297-cohete-mano-dibujada-en-tiza-vector-icono-aislado-para-web-m%C3%B3vil-e-infograf%C3%ADa-.jpg?ver=6" alt="" srcset="">
-          <!-- <svg xmlns="http://www.w3.org/2000/svg" 
-            viewBox="0 0 512 512">
-            <path d="M331.8 224.1c28.29 0 54.88 10.99 74.86 30.97l19.59 19.59c40.01-17.74 71.25-53.3 81.62-96.65c5.725-23.92 5.34-47.08 .2148-68.4c-2.613-10.88-16.43-14.51-24.34-6.604l-68.9 68.9h-75.6V97.2l68.9-68.9c7.912-7.912 4.275-21.73-6.604-24.34c-21.32-5.125-44.48-5.51-68.4 .2148c-55.3 13.23-98.39 60.22-107.2 116.4C224.5 128.9 224.2 137 224.3 145l82.78 82.86C315.2 225.1 323.5 224.1 331.8 224.1zM384 278.6c-23.16-23.16-57.57-27.57-85.39-13.9L191.1 158L191.1 95.99l-127.1-95.99L0 63.1l96 127.1l62.04 .0077l106.7 106.6c-13.67 27.82-9.251 62.23 13.91 85.39l117 117.1c14.62 14.5 38.21 14.5 52.71-.0016l52.75-52.75c14.5-14.5 14.5-38.08-.0016-52.71L384 278.6zM227.9 307L168.7 247.9l-148.9 148.9c-26.37 26.37-26.37 69.08 0 95.45C32.96 505.4 50.21 512 67.5 512s34.54-6.592 47.72-19.78l119.1-119.1C225.5 352.3 222.6 329.4 227.9 307zM64 472c-13.25 0-24-10.75-24-24c0-13.26 10.75-24 24-24S88 434.7 88 448C88 461.3 77.25 472 64 472z"/>
-          </svg> -->
-        </div>
-        <div class="col-md">
-          <img src="https://media.istockphoto.com/photos/fountain-pen-signature-picture-id485827074?k=20&m=485827074&s=612x612&w=0&h=3E1wvSupDtQMuwHn2beoOvYhUhm2z9KjGKgkeaMoG14=" alt="" srcset="">
-          <!-- <svg xmlns="http://www.w3.org/2000/svg" 
-            viewBox="0 0 512 512">
-            <path d="M331.8 224.1c28.29 0 54.88 10.99 74.86 30.97l19.59 19.59c40.01-17.74 71.25-53.3 81.62-96.65c5.725-23.92 5.34-47.08 .2148-68.4c-2.613-10.88-16.43-14.51-24.34-6.604l-68.9 68.9h-75.6V97.2l68.9-68.9c7.912-7.912 4.275-21.73-6.604-24.34c-21.32-5.125-44.48-5.51-68.4 .2148c-55.3 13.23-98.39 60.22-107.2 116.4C224.5 128.9 224.2 137 224.3 145l82.78 82.86C315.2 225.1 323.5 224.1 331.8 224.1zM384 278.6c-23.16-23.16-57.57-27.57-85.39-13.9L191.1 158L191.1 95.99l-127.1-95.99L0 63.1l96 127.1l62.04 .0077l106.7 106.6c-13.67 27.82-9.251 62.23 13.91 85.39l117 117.1c14.62 14.5 38.21 14.5 52.71-.0016l52.75-52.75c14.5-14.5 14.5-38.08-.0016-52.71L384 278.6zM227.9 307L168.7 247.9l-148.9 148.9c-26.37 26.37-26.37 69.08 0 95.45C32.96 505.4 50.21 512 67.5 512s34.54-6.592 47.72-19.78l119.1-119.1C225.5 352.3 222.6 329.4 227.9 307zM64 472c-13.25 0-24-10.75-24-24c0-13.26 10.75-24 24-24S88 434.7 88 448C88 461.3 77.25 472 64 472z"/>
-          </svg> -->
-        </div>
-        <div class="col-md">
-          <img src="https://us.123rf.com/450wm/rastudio/rastudio1604/rastudio160406624/55076297-cohete-mano-dibujada-en-tiza-vector-icono-aislado-para-web-m%C3%B3vil-e-infograf%C3%ADa-.jpg?ver=6" alt="" srcset="">
-          <!-- <svg xmlns="http://www.w3.org/2000/svg" 
-            viewBox="0 0 512 512">
-            <path d="M331.8 224.1c28.29 0 54.88 10.99 74.86 30.97l19.59 19.59c40.01-17.74 71.25-53.3 81.62-96.65c5.725-23.92 5.34-47.08 .2148-68.4c-2.613-10.88-16.43-14.51-24.34-6.604l-68.9 68.9h-75.6V97.2l68.9-68.9c7.912-7.912 4.275-21.73-6.604-24.34c-21.32-5.125-44.48-5.51-68.4 .2148c-55.3 13.23-98.39 60.22-107.2 116.4C224.5 128.9 224.2 137 224.3 145l82.78 82.86C315.2 225.1 323.5 224.1 331.8 224.1zM384 278.6c-23.16-23.16-57.57-27.57-85.39-13.9L191.1 158L191.1 95.99l-127.1-95.99L0 63.1l96 127.1l62.04 .0077l106.7 106.6c-13.67 27.82-9.251 62.23 13.91 85.39l117 117.1c14.62 14.5 38.21 14.5 52.71-.0016l52.75-52.75c14.5-14.5 14.5-38.08-.0016-52.71L384 278.6zM227.9 307L168.7 247.9l-148.9 148.9c-26.37 26.37-26.37 69.08 0 95.45C32.96 505.4 50.21 512 67.5 512s34.54-6.592 47.72-19.78l119.1-119.1C225.5 352.3 222.6 329.4 227.9 307zM64 472c-13.25 0-24-10.75-24-24c0-13.26 10.75-24 24-24S88 434.7 88 448C88 461.3 77.25 472 64 472z"/>
-          </svg> -->
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <?php include('footer-temp.html'); ?>
 
   <!-- Bootstrap JavaScript Libraries -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+  <!-- locomotive -->
+  <script src="https://cdn.jsdelivr.net/npm/locomotive-scroll@4.1.1/dist/locomotive-scroll.min.js"></script>
+  <script>
+    const scroll = new LocomotiveScroll({
+      el: document.querySelector('[data-scroll-container]'),
+      smooth:true,
+      tablet: {smooth:true},
+      smartphone: {smooth:true}
+    })
+  </script>
+
 </body>
 
 </html>
